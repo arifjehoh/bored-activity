92257a657c2ed34587d30d9c3b75a3f1809c5f58
Arif Jehda-Oh <arifjeh.oh@gmail.com>
2021-12-01 00:33:58 +0100

    #9: fix: Seperated the style component to a file.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt='Vue logo' src='./assets/logo.png' />
+    <HelloWorld msg='Welcome to Your Vue.js App' />
+  </div>
 </template>
-
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import '@/style.css'
 
 export default {
   name: 'App',
@@ -15,12 +17,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
