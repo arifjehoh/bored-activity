f6e54018ef503bd4354e0f97cc61251b63b8a6f7
Arif Jehda-Oh <arifjeh.oh@gmail.com>
2021-12-05 16:35:24 +0100

    #26: feat: Creating skeleton for GameRoom View-Presenter

---
+++ src/components/presenters/GameRoomPresenter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Game Room Presenter</h1>
+    <GameRoom />
+  </div>
+</template>
+
+<script>
+import GameRoom from '../views/GameRoomView.vue'
+
+export default {
+  name: 'GameRoomPresenter',
+  components: {
+    GameRoom
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/views/GameRoomView.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Game Room</h1>
+</template>
+
+<script>
+export default {
+  name: 'GameRoom'
+}
+</script>
+
+<style scoped>
+</style>
