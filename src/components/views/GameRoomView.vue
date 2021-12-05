2e0931ea6cb53c9545a70b374d129ab3099887da
Arif Jehda-Oh <arifjeh.oh@gmail.com>
2021-12-05 16:59:25 +0100

    26: add: Dynamically show Game Room Data

--- src/components/views/GameRoomView.vue
+++ src/components/views/GameRoomView.vue
@@ -1,10 +1,28 @@
 <template>
-  <h1>Game Room</h1>
+  <div>
+    <h1>{{ title }}</h1>
+    <h2>{{ room_status }}</h2>
+    <h3>{{ participants }}</h3>
+    {{ activities }}
+    <Button type='button'>End game room</button>
+    <Button type='button'>Leave game room</button>
+  </div>
+
 </template>
 
 <script>
+import MockGameRoom from '../models/MockGameRoom.js'
+
 export default {
-  name: 'GameRoom'
+  name: 'GameRoom',
+  data () {
+    return {
+      title: MockGameRoom.title,
+      room_status: MockGameRoom.room_status,
+      participants: MockGameRoom.participants,
+      activities: MockGameRoom.activities
+    }
+  }
 }
 </script>
 
