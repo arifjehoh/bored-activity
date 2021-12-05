365ff2d2be46a7c48cae7068ce74a335f9799336
Arif Jehda-Oh <arifjeh.oh@gmail.com>
2021-12-05 17:48:35 +0100

    #26: add: Update the UI with data

--- src/components/views/GameRoomView.vue
+++ src/components/views/GameRoomView.vue
@@ -1,11 +1,22 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <h2>{{ room_status }}</h2>
-    <h3>{{ participants }}</h3>
-    {{ activities }}
-    <Button type='button'>End game room</button>
-    <Button type='button'>Leave game room</button>
+  <div id='container'>
+    <div id='header'>
+      <h1>{{ title }} - {{ room_status }}</h1>
+    </div>
+    <div id='content'>
+      <div id='gameBoard'>
+        <div v-for='activity in activities' v-bind:key='activity.name'>{{ activity.name }}</div>
+      </div>
+      <div id='participants'>
+        <h2>Participants:</h2>
+        <div v-for='participant in participants' v-bind:key='participant.name'>
+          {{ participant.name }}: {{ participant.score }}
+        </div>
+        <Button type='button'>Join game room</button>
+        <Button type='button'>End game room</button>
+        <Button type='button'>Leave game room</button>
+      </div>
+    </div>
   </div>
 
 </template>
@@ -27,4 +38,28 @@ export default {
 </script>
 
 <style scoped>
+#container {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+}
+
+#content {
+  display: flex;
+  flex-direction: row;
+  flex: 40%;
+}
+
+#participants {
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+}
+
+#gameBoard {
+  display: grid;
+  grid-template-columns: auto auto auto auto auto;
+  grid-gap: 10px;
+  flex: 60%;
+}
 </style>
