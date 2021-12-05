7ee528485c1a0e888bb67af442ad6f00f0bcc249
Nilfal <78425350+Nilfal@users.noreply.github.com>
2021-12-05 20:33:10 +0100

    #20: feat: Created a sign in page

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <img alt='Vue logo' src='./assets/logo.png' />
-    <HelloWorld msg='Welcome to Your Vue.js App' />
+    <LogIn/>
   </div>
 </template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 import '@/style.css'
+import LogIn from './components/views/LogIn.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LogIn
   }
 }
 </script>
