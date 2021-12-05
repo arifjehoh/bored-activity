15e1a7de8952c1c2499de5e06d1a11fce56a69ab
Nilfal <78425350+Nilfal@users.noreply.github.com>
2021-12-05 21:10:40 +0100

    #20: fix:

--- src/components/views/LogIn.vue
+++ src/components/views/LogIn.vue
@@ -1,58 +1,33 @@
 <template>
-  <div id = 'container' >
-      <h3 id = 'siteName' >Bored Bingo</h3>
-      <div id = 'rightMenu'>
-      <button type='button' v-on:click='goToGame'> Game </button>
-      <button type='button' v-on:click='goToRoom' > Room </button>
-      <button type='button' v-on:click='goToList'> List </button>
-      <button type='button' v-on:click='goToSignIn'> Sign in </button>
-      </div>
 
-      <div id = 'midContent'>
-        <h1> Bored Bingo</h1>
-        <h4> Sign in </h4>
-      </div>
+  <div id="content">
+    <div id="header">
+      <h1>Bored Bingo</h1>
+      <h2>Sign in</h2>
+    </div>
 
-      <div>
-        <h4> Email Adress </h4>
-        <input placeholder>
-        <h4> Password</h4>
-        <input placeholder >
-       </div>
-        <div id = 'profileButton'><button type = 'button' v-on:click='SignIn'> Sign in </button></div>
-         <div id = 'loginButton'><button type='button' v-on:click='createProfile'> Create Profile </button></div>
+    <div>
+      <h4>Email Adress</h4>
+      <input type="email" placeholder  id= 'place'/>
+      <h4>Password</h4>
+      <input type="password"> placeholder id= 'place' />
+    </div>
+    <div id="profileButton">
+      <button id="btnSignin" v-on:click="SignIn">Sign in</button>
+    </div>
+    <div id="loginButton">
+      <button id="btnSignin" v-on:click="createProfile">Create Profile</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'LogIn',
-  props: {
-    LogIn: Object
-  },
   data () {
-    return {
-      title: 'No title',
-      room_status: 'Not started',
-      participants: [],
-      activities: []
-    }
   },
-  mounted () {
-  },
+  mounted () {},
   methods: {
-    goToGame: function () {
-      console.log('User wants to go to game.')
-    },
-    goToRoom: function () {
-      console.log('User wants to go to room.')
-    },
-    goToList: function () {
-      console.log('User wants to go to list.')
-    },
-    goToSignIn: function () {
-      console.log('User wants to go to sign in.')
-    },
     SignIn: function () {
       console.log('User wants to  sign in.')
     },
@@ -64,36 +39,41 @@ export default {
 </script>
 
 <style scoped>
-#container {
+#content {
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
 }
-#siteName{
+#siteName {
   text-align: left;
   text-align: top;
   position: relative;
-  bottom: 50px;;
+  bottom: 50px;
 }
-#rightMenu{
+#rightMenu {
   text-align: end;
   text-align: top;
   text-size-adjust: 50%;
-   position: relative;
-  bottom: 95px;;
+  position: relative;
+  bottom: 95px;
 }
-#midContent{
+#midContent {
   text-align: center;
   position: relative;
-  bottom: 80px;;
+  bottom: 80px;
 }
-#profileButton{
+#profileButton {
   position: relative;
-  top: 10px;;
+  top: 10px;
 }
-#loginButton{
+#loginButton {
   position: relative;
-  top: 10px;;
+  top: 10px;
 }
-
+#btnSignin{
+  font-size: 20px;
+}
+#place{
+  font-size: 20px;
+}
 </style>
