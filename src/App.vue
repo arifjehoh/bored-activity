240d497164b7bb6072230bc3d3eec77dc277c321
Arif Jehda-Oh <arifjeh.oh@gmail.com>
2021-12-05 16:35:58 +0100

    #26: add: Add connection for GameRoomPresenter to App

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div>
     <img alt='Vue logo' src='./assets/logo.png' />
     <HelloWorld msg='Welcome to Your Vue.js App' />
+    <GameRoomPresenter />
   </div>
 </template>
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import GameRoomPresenter from './components/presenters/GameRoomPresenter.vue'
 import '@/style.css'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    GameRoomPresenter
   }
 }
 </script>
