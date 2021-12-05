bdb030d73250c768b3c9e77c6f6d88d254efc87c
Arif Jehda-Oh <arifjeh.oh@gmail.com>
2021-12-05 23:52:37 +0100

    #26: add: Temporary Winning logic

--- src/components/models/GameRoomModel.js
+++ src/components/models/GameRoomModel.js
@@ -5,6 +5,13 @@ class GameRoom {
     this.participants = participants
     this.activities = activities
   }
+
+  whoIsTheWinner () {
+    this.participants.sort(function (a, b) {
+      return b.score - a.score
+    })
+    return this.participants[0]
+  }
 }
 
 export default GameRoom
