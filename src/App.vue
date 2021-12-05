0c324e0187c196c1a969df3363ff91a1e34f62b6
Abdullah Trabulsiah <abdtra@kth.se>
2021-12-05 14:00:26 +0100

    #19: feat: Created a new component "ActivityList" #19: add: added a new component "ActivityList" to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <div>
+  <div id=app> 
     <img alt='Vue logo' src='./assets/logo.png' />
     <HelloWorld msg='Welcome to Your Vue.js App' />
+    <h1>Fetch data with api</h1>
+    <ActivityList />
   </div>
 </template>
 <script>
+import Activitylist from './components/ActivityList.vue'
 import HelloWorld from './components/HelloWorld.vue'
 import '@/style.css'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ActivityList
   }
 }
 </script>
---
+++ src/components/ActivityList.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1>Activity List</h1>
+        <table border="1px">
+            <tr>
+                <td>type </td>
+                <td>participants </td>
+                <td>key </td>
+             </tr>
+
+             <tr v-for="item in list" v-bind:key= "item.key">
+                <td>{{item.type}} </td>
+                <td>{{item.participants}} </td>
+                <td>{{item.key}}  </td>
+            </tr>
+    </table>
+
+    </div>
+</template>
+<script>
+import Vue from 'vue';
+import axios from 'axios'
+import VueAxios from 'vuew-axios'
+Vue.use(VueAxios, axios)
+export default {
+    name: "ActivityList",
+    data(){
+        return{list:undefined}
+    },
+    mounted(){
+        Vue.axios.get('https://www.boredapi.com/api/activity')
+        .then((r) => {
+            this.list=r.activity;
+            console.warn(r.activity)
+        })
+    }
+}
+</script>
