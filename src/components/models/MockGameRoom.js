ed9899d1e2788382bd6c805c0088a754ed1ce994
Arif Jehda-Oh <arifjeh.oh@gmail.com>
2021-12-05 16:58:51 +0100

    26: feat: Creating mock data for Game Room View

---
+++ src/components/models/MockGameRoom.js
@@ -0,0 +1,24 @@
+const MockGameRoom = {
+  title: 'Game Room Title 1',
+  room_status: 'Active',
+  participants: [
+    {
+      name: 'Arif'
+    }
+  ],
+  activities: [
+    {
+      name: 'Activity 1'
+    },
+    {
+      name: 'Activity 2'
+    },
+    {
+      name: 'Activity 3'
+    },
+    {
+      name: 'Activity 4'
+    }
+  ]
+}
+export default MockGameRoom
