f9d8581d8684e1151f574613fea0fe7664624a21
Nilfal <78425350+Nilfal@users.noreply.github.com>
2021-12-05 21:13:23 +0100

    #20:fix: Last fix

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <div>
+    <img alt='Vue logo' src='./assets/logo.png' />
+    <HelloWorld msg='Welcome to Your Vue.js App' />
     <LogIn/>
+    
   </div>
 </template>
 <script>
 
 import '@/style.css'
+import HelloWorld from './components/HelloWorld.vue'
 import LogIn from './components/views/LogIn.vue'
 
 export default {
   name: 'App',
   components: {
-    LogIn
-  }
+  HelloWorld,
+  LogIn
+}
 }
 </script>
 
