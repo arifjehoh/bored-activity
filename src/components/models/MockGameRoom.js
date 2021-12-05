1464ac9e58e188d7c958683931f86bacc76bc561
Arif Jehda-Oh <arifjeh.oh@gmail.com>
2021-12-05 18:28:09 +0100

    #26: add: Update MockDataRoom with boredApi data

--- src/components/models/MockGameRoom.js
+++ src/components/models/MockGameRoom.js
@@ -17,35 +17,37 @@ const MockGameRoom = {
   ],
   activities: [
     {
-      name: 'Activity 1'
+      activity: 'Start a webinar on a topic of your choice',
+      type: 'education',
+      link: '',
+      key: '6826029'
     },
     {
-      name: 'Activity 2'
+      activity: 'Learn how to fold a paper crane',
+      type: 'education',
+      participants: 1,
+      price: 0.1,
+      link: '',
+      key: '3136036',
+      accessibility: 0.05
     },
     {
-      name: 'Activity 3'
+      activity: 'Clean out your refrigerator',
+      type: 'busywork',
+      participants: 1,
+      price: 0,
+      link: '',
+      key: '9324336',
+      accessibility: 0
     },
-    {
-      name: 'Activity 4'
-    },
-    {
-      name: 'Activity 5'
-    },
-    {
-      name: 'Activity 6'
-    },
-    {
-      name: 'Activity 7'
-    },
-    {
-      name: 'Activity 8'
-    },
-    {
-      name: 'Activity 9'
-    },
-    {
-      name: 'Activity 10'
-    }
+    { activity: 'Play a game of tennis with a friend', type: 'social', participants: 2, price: 0.1, link: '', key: '1093640', accessibility: 0.4 },
+    { activity: 'Play a game of Monopoly', type: 'social', participants: 4, price: 0.2, link: '', key: '1408058', accessibility: 0.3 },
+    { activity: 'Make homemade ice cream', type: 'cooking', participants: 1, price: 0.2, link: '', key: '3818400', accessibility: 0.2 },
+    { activity: 'Visit your past teachers', type: 'social', participants: 1, price: 0, link: '', key: '8238918', accessibility: 0.7 },
+    { activity: 'Learn woodworking', type: 'diy', participants: 1, price: 0.3, link: '', key: '9216391', accessibility: 0.3 },
+    { activity: 'Create a meal plan for the coming week', type: 'cooking', participants: 1, price: 0, link: '', key: '3491470', accessibility: 0 },
+    { activity: 'Clean out your garage', type: 'busywork', participants: 1, price: 0, link: '', key: '7023703', accessibility: 0.1 },
+    { activity: 'Make a simple musical instrument', type: 'music', participants: 1, price: 0.4, link: '', key: '7091374', accessibility: 0.25 }
   ]
 }
 export default MockGameRoom
