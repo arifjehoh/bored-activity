262aac694bd2abe790630716d9feec169f41818a
Abdullah Trabulsiah <abdtra@kth.se>
2021-12-06 16:15:36 +0100

    #19: feat: Created a new component "ActivityList"

--- src/components/ActivityList.vue
+++ src/components/ActivityList.vue
@@ -1,38 +1,30 @@
 <template>
-    <div>
-        <h1>Activity List</h1>
-        <table border="1px">
-            <tr>
-                <td>type </td>
-                <td>participants </td>
-                <td>key </td>
-             </tr>
+  <div>
+    <h1>Activity List</h1>
+    <table border='1px'>
+      <tr>
+        <td>type</td>
+        <td>participants</td>
+        <td>key</td>
+      </tr>
 
-             <tr v-for="item in list" v-bind:key= "item.key">
-                <td>{{item.type}} </td>
-                <td>{{item.participants}} </td>
-                <td>{{item.key}}  </td>
-            </tr>
+      <tr v-for='item in list' v-bind:key='item.key'>
+        <td>{{ item.type }}</td>
+        <td>{{ item.participants }}</td>
+        <td>{{ item.key }}</td>
+      </tr>
     </table>
-
-    </div>
+  </div>
 </template>
 <script>
-import Vue from 'vue';
 import axios from 'axios'
-import VueAxios from 'vuew-axios'
-Vue.use(VueAxios, axios)
 export default {
-    name: "ActivityList",
-    data(){
-        return{list:undefined}
-    },
-     mounted(){
-        Vue.axios.get('https://www.boredapi.com/api/activity')
-         .then((r) => {
-            this.list = r.activity;
-            console.warn(r.activity)
-        })
-     }
+  name: 'ActivityList',
+  data () {
+    return { list: [] }
+  },
+  mounted () {
+    axios.get('https://www.boredapi.com/api/activity').then(data => { console.log(data.data); this.list = [...this.list, (data.data)] }).catch(console.error)
+  }
 }
 </script>
