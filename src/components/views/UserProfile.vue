fa3e9403fc5ea4afd844b2bcd24add8108428c3e
Arif Jehda-Oh <arifjeh.oh@gmail.com>
2021-12-09 20:14:40 +0100

    #29: Refactored

--- src/components/views/UserProfile.vue
+++ src/components/views/UserProfile.vue
@@ -1,19 +1,20 @@
 <template>
- <div>
-        <div>
-<h1> Profile</h1>
-            <div>
-            <h4>{{this.name}}</h4>
-            <h5>{{this.email}}</h5>
-            <button id='btnLogOut' v-on:click= 'LogOut()'>Logout</button>
-             </div>
-        </div>
-     </div>
+  <div>
+    <div>
+      <h1>Profile</h1>
+      <div>
+        <h4>{{ this.name }}</h4>
+        <h5>{{ this.email }}</h5>
+        <button id='btnLogOut' v-on:click='LogOut()'>Logout</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { app } from '../utils/firebaseConfig.js'
+import app from '../utils/firebaseConfig.js'
 import { getAuth, signOut } from 'firebase/auth'
+const auth = getAuth(app)
 
 export default {
   name: 'UserProfile',
@@ -26,12 +27,12 @@ export default {
   methods: {
     LogOut: function () {
       console.log('User wants to  sign out.')
-
-      const auth = getAuth(app)
-      signOut(auth, this.email, this.name)
-      signOut(auth).then(() => {
-      }).catch(
-      )
+      signOut(auth)
+        .then(() => {
+          console.log('User is logged out')
+          console.log('TODO Navigate to front page')
+        })
+        .catch((error) => console.error(error))
     }
   }
 }
