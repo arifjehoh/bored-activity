fe30cbf1adaeddfb3fde23f6433367c293022831
Nilfal <78425350+Nilfal@users.noreply.github.com>
2021-12-09 13:19:02 +0100

    #44:feat : reset passwrod

---
+++ src/components/views/ResetPassword.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id='content'>
+    <div id='header'>
+      <h1>Bored Bingo</h1>
+      <h2>Reset password</h2>
+    </div>
+
+    <div>
+      <h4>Email Adress</h4>
+      <input type='email' placeholder="Enter email" v-model = "email" id='place' />
+    </div>
+    <div id='resetPassword'>
+      <button id='btnSignin' v-on:click='ResetPassword'>Reset Passwrod</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth'
+import { app } from '../utils/firebaseConfig'
+export default {
+  name: 'ResetPassword',
+  data () {
+    return {
+      email: null
+    }
+  },
+  methods: {
+    ResetPassword: function () {
+      console.log('User wants to reset password.')
+      console.log(app)
+      const auth = getAuth()
+      sendPasswordResetEmail(auth, this.email)
+        .then(() => {
+          // Password reset email sent!
+          // ..
+        })
+        .catch((error) => {
+          const errorCode = error.code
+          const errorMessage = error.message
+          console.log(errorCode, errorMessage)
+          // ..
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+#content {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+}
+#midContent {
+  text-align: center;
+  position: relative;
+  bottom: 80px;
+}
+#loginButton {
+  position: relative;
+  top: 10px;
+}
+#btnSignin {
+  font-size: 20px;
+}
+#place {
+  font-size: 20px;
+}
+</style>
