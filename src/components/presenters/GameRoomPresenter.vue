939b8d858f060d0813e739016c4b5234788a13ca
Arif Jehda-Oh <arifjeh.oh@gmail.com>
2021-12-10 00:05:47 +0100

    #26: add: Show GameRoomView if data is fetched

--- src/components/presenters/GameRoomPresenter.vue
+++ src/components/presenters/GameRoomPresenter.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <h1>Game Room Presenter</h1>
-    <GameRoom :gameRoom='gameRoom' />
+    <GameRoom :gameRoom='gameRoom' v-if='haveFetched'/>
   </div>
 </template>
 
 <script>
 import GameRoom from '../views/GameRoomView.vue'
-import MockGameRoom from '../models/MockGameRoom.js'
 import GameRoomModel from '../models/GameRoomModel.js'
+import getGameRoom from '../utils/FirebaseService.js'
 
 export default {
   name: 'GameRoomPresenter',
@@ -17,8 +17,19 @@ export default {
   },
   data () {
     return {
-      gameRoom: new GameRoomModel(MockGameRoom)
+      gameRoom: new GameRoomModel(),
+      haveFetched: false
     }
+  },
+  beforeCreate () {
+    this.haveFetched = false
+    getGameRoom().then(data => {
+      this.gameRoom.setTitle(data.title)
+      this.gameRoom.setStatus(data.status)
+      this.gameRoom.setParticipants(data.participants)
+      this.gameRoom.setActivities(data.activities)
+      this.haveFetched = true
+    }).catch(console.error)
   }
 }
 </script>
