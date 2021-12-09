3f51ccbb748ff21dbb376d3323bd650dbe186f55
Murtadha Alobaidi <69900487+mhaao@users.noreply.github.com>
2021-12-09 17:15:10 +0100

    #29: feat: Created Profile 'UserProfile.vue' #33:feat: Created Profile 'UserProfile.vue'

---
+++ src/components/views/UserProfile.vue
@@ -0,0 +1,38 @@
+<template>
+ <div>
+        <div>
+<h1> Profile</h1>
+            <div>
+            <h4>{{this.name}}</h4>
+            <h5>{{this.email}}</h5>
+            <button id='btnLogOut' v-on:click= 'LogOut()'>Logout</button>
+             </div>
+        </div>
+     </div>
+</template>
+
+<script>
+import { app } from '../utils/firebaseConfig.js'
+import { getAuth, signOut } from 'firebase/auth'
+
+export default {
+  name: 'UserProfile',
+  data () {
+    return {
+      name: null,
+      email: null
+    }
+  },
+  methods: {
+    LogOut: function () {
+      console.log('User wants to  sign out.')
+
+      const auth = getAuth(app)
+      signOut(auth, this.email, this.name)
+      signOut(auth).then(() => {
+      }).catch(
+      )
+    }
+  }
+}
+</script>
