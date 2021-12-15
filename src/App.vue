3f3f1b6af9bdede7b86f42725e5ee77234fda008
Arif Jehda-Oh <arifjeh.oh@gmail.com>
2021-12-15 20:48:24 +0100

    #30: refactor navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,17 @@
 <template>
   <div>
-    <router-link to="/rooms">Go to Home</router-link>
-    <router-link to="/profile">Profile</router-link>
-    <router-link to="/sign-in">Sign in</router-link>
+    <NavigationPresenter />
     <router-view />
-    <LogInPresenter />
-    <CreateAccountPresenter/>
-    <GameRoomListPresenter/>
-    <CreateGameRoomPresenter />
-    <ResetPasswordPresenter />
-    <UserProfilePresenter />
-    <GameRoomPresenter />
   </div>
 </template>
 <script>
-import UserProfilePresenter from './components/presenters/UserProfilePresenter.vue'
-import CreateGameRoomPresenter from './components/presenters/CreateGameRoomPresenter.vue'
-import GameRoomPresenter from './components/presenters/GameRoomPresenter.vue'
-import GameRoomListPresenter from './components/presenters/GameRoomListPresenter.vue'
-import LogInPresenter from './components/presenters/LogInPresenter.vue'
-import ResetPasswordPresenter from './components/presenters/ResetPasswordPresenter.vue'
-import CreateAccountPresenter from './components/presenters/CreateAccountPresenter.vue'
+import NavigationPresenter from './components/presenters/NavigationPresenter.vue'
 import '@/style.css'
 
 export default {
   name: 'App',
   components: {
-    LogInPresenter,
-    CreateAccountPresenter,
-    UserProfilePresenter,
-    CreateGameRoomPresenter,
-    GameRoomPresenter,
-    GameRoomListPresenter,
-    ResetPasswordPresenter
+    NavigationPresenter
   }
 }
 </script>
---
+++ src/components/presenters/NavigationPresenter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <router-link to='/rooms'>Go to Home</router-link>
+    <router-link v-if='true' to='/profile'>Profile</router-link>
+    <router-link v-if='false' to='/sign-in'>Sign in</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavigationPresenter'
+}
+</script>
+
+<style>
+</style>
