039cfcc3ebe1da61172fa4e866254478990ad030
Arif Jehda-Oh <arifjeh.oh@gmail.com>
2021-12-17 00:09:31 +0100

    #94: add bootstrap to Navigation bar

--- src/components/presenters/NavigationPresenter.vue
+++ src/components/presenters/NavigationPresenter.vue
@@ -1,9 +1,21 @@
 <template>
-  <div>
-    <router-link to='/rooms'>Go to Home</router-link>
-    <router-link v-if='this.$store.state.user' to='/profile'>Profile</router-link>
-    <router-link v-if='!this.$store.state.user' to='/sign-in'>Sign in</router-link>
-  </div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Bingo when you are bored</a>
+    <div class="">
+      <ul class="navbar-nav">
+        <!-- TODO LINKS SHOULD BE TO THE LEFT -->
+        <li class="nav-item active">
+          <router-link class="nav-item active" to='/rooms'>Game room list</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-item" v-if='this.$store.state.user' to='/profile'>Profile</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-item" v-if='!this.$store.state.user' to='/sign-in'>Sign in</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
