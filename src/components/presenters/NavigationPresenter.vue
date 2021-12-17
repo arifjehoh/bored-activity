0f2377a12607a7d23463f4c06ac8db579eaad001
Arif Jehda-Oh <arifjeh.oh@gmail.com>
2021-12-17 23:20:47 +0100

    #94: ref: Refactored navigation bar css

--- src/components/presenters/NavigationPresenter.vue
+++ src/components/presenters/NavigationPresenter.vue
@@ -1,50 +1,42 @@
 <template>
-  <nav class="navbar navbar-dark bg-primary ">
-    <a id='rightContent' class="navbar-brand btn-link" href="#" v-on:click='toHomepage'>Bingo when you are bored</a>
-      <ul class="navbar-nav">
-        <!-- TODO LINKS SHOULD BE TO THE LEFT -->
-        <li class="nav-item active">
-          <router-link  id='leftContent' class="nav-item active" to='/rooms'>Game room list</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link  id='leftContent1' class="nav-item" v-if='this.$store.state.user' to='/profile'>Profile</router-link>
-        </li>
-        <li id='leftContent' class="nav-item">
-          <router-link id='leftContent1' class="nav-item" v-if='!this.$store.state.user' to='/sign-in'>Sign in</router-link>
-        </li>
-      </ul>
+  <nav class="navbar navbar-dark bg-primary">
+    <div class='left nav-content' >
+      <router-link class="navbar-brand btn-link text-white" to='/rooms'><b>Bingo when you are bored</b></router-link>
+    </div>
+    <div class='right nav-content'>
+      <div class="row">
+        <span class="nav-item col">
+          <router-link class='text-white' to="/rooms">Game room list</router-link>
+        </span>
+        <span class="nav-item col" v-if="this.$store.state.user">
+          <router-link class='text-white' to="/profile">Profile</router-link>
+        </span>
+        <span class="nav-item col" v-if="!this.$store.state.user">
+          <router-link class='text-white' to='/sign-in'>Sign in</router-link>
+        </span>
+      </div>
+    </div>
   </nav>
 </template>
 
 <script>
 export default {
-  name: 'NavigationPresenter',
-  methods: {
-    toHomepage: function () {
-      this.$router.push('/')
-    }
-  }
+  name: 'NavigationPresenter'
 }
 </script>
 
 <style scoped>
-#leftContent{
-  color: black;
+.left {
   align-content: left;
-  font-weight: 400;
-  font-size: 20px;
-  line-height: 30px;
-  text-decoration-color: rgba(0,0,0.9);
-  text-decoration-line: none;
-  text-decoration-style: solid;
-  text-decoration-thickness: 100%;
-  text-align: center;
-  position: left;
-  color: white;
 }
-#leftContent1{
-  color: black;
-  align-content: left;
+
+.right {
+  align-content: right;
+}
+
+.nav-content {
+  overflow: hidden;
+  white-space: nowrap;
   font-weight: 400;
   font-size: 20px;
   line-height: 30px;
@@ -52,11 +44,6 @@ export default {
   text-decoration-line: none;
   text-decoration-style: solid;
   text-decoration-thickness: 100%;
-  text-align: center;
-  margin-left: 20px;
-  color: white;
-}#rightContent{
-  margin-right:60%
 }
 
 </style>
