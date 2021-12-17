e6664c591a7129587e22a7f02efe46f082c2f992
Arif Jehda-Oh <arifjeh.oh@gmail.com>
2021-12-17 01:36:57 +0100

    Update CreateGameRoomView.vue

--- src/components/views/CreateGameRoomView.vue
+++ src/components/views/CreateGameRoomView.vue
@@ -1,11 +1,34 @@
 <template>
-  <div>
-    <h1>Create Game Room</h1>
-    <div>
-    <label id='label' for='roomName'>Game room name:</label>
+  <div class="container">
+    <div class="card">
+      <h1 class="card-header">Create Game Room</h1>
+      <form>
+        <div class="form-group">
+          <label for='roomName'>Game room name:</label>
+          <input class='form-control' type='text' id='roomName' name='roomName' v-model='roomName' placeholder="Type of activities:" aria-describedby="emailHelp"/>
+        </div>
+        <div class="form-group">
+          <label for='typeActivites'>Type of activities:</label>
+          <select class='form-control' name='typeActivites' v-model='type'>
+            <option value=''>Everything</option>
+            <option value='education'>Education</option>
+            <option value='recreational'>Recreational</option>
+            <option value='social'>Social</option>
+            <option value='diy'>Diy</option>
+            <option value='charity'>Charity</option>
+            <option value='cooking'>Cooking</option>
+            <option value='relaxation'>Relaxation</option>
+            <option value='music'>Music</option>
+            <option value='busywork'>Busywork</option>
+          </select>
+        </div>
+        <div class="form-group">
+          <button type="submit" class="btn btn-primary" v-on:click='createGame'>Create game room</button>
+        </div>
+      </form>
     </div>
-    <input type='text' id='roomName' name='roomName' v-model='roomName' placeholder="Type of activities:" />
-    <div>
+  </div>
+    <!-- <div>
     <label id ='label' for='typeActivites'>Type of activities:</label>
     <div>
     <select id='typeActivites' name='typeActivites' v-model='type'>
@@ -25,7 +48,7 @@
     <div>
     <button v-on:click='createGame'>Create game room</button>
     </div>
-  </div>
+  </div> -->
 </template>
 
 <script>
@@ -48,38 +71,4 @@ export default {
 </script>
 
 <style scoped>
-button{
-  margin: 10px;
-border-radius: 8px;
-width: 15%;
-height: 50px;
-}
-button:active {
-  font-weight: bold;
-  background-color: #d8d8d8;
-  box-shadow: 0 2px #666;
-  transform: translateY(0.5px);
-}
-#typeActivites{
-  margin: 10px;
-  padding: 8px;
-  border-radius: 8px;
-  width: flex;
-  font-size: 20px;
-  font-weight: bold;
-
-}
-#label{
-  margin: 10px;
-  font-size: 20px;
-  font-weight: bold;
-}
-#roomName{
-  margin: 10px;
-  padding: 8px;
-  border-radius: 8px;
-  width: flex;
-  font-size: 20px;
-  font-weight: bold;
-}
 </style>
