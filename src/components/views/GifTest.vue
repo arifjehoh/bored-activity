505807acb9f0817b76b32187c32dec61f3f96ee2
Arif Jehda-Oh <arifjeh.oh@gmail.com>
2021-12-18 01:39:53 +0100

    adding height and with

--- src/components/views/GifTest.vue
+++ src/components/views/GifTest.vue
@@ -1,15 +1,19 @@
 <template>
 <div id ='test'>
   <h1> WINNER WINNER CHICKEN DINNER </h1>
- <div><iframe id='test' src="https://giphy.com/embed/Vuw9m5wXviFIQ"  frameBorder="0"></iframe></div>
+ <div ><iframe class='content' src="https://giphy.com/embed/Vuw9m5wXviFIQ"  frameBorder="0"></iframe></div>
 </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style scoped>
+.content {
+  margin-left: auto;
+  margin-right: auto;
+  height: 640px;
+  width: 640px;
+}
 </style>
